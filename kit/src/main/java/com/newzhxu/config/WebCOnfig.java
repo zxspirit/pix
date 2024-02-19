bb1893cde513bb2e7229c0e01e050f382d42d29e
zahl <zheng2580369@gmail.com>
2024-02-20 00:19:43 +0800

    测试下closeable

--- kit/src/main/java/com/newzhxu/config/WebCOnfig.java
+++ kit/src/main/java/com/newzhxu/config/WebCOnfig.java
@@ -1,14 +1,28 @@
 package com.newzhxu.config;
 
 import com.newzhxu.interceptor.MyInterceptor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.io.Closeable;
+import java.io.IOException;
 
 @Configuration
 public class WebCOnfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new MyInterceptor()).addPathPatterns("/*");
+    }
+    @Bean
+    public A a() {
+        return new A();
+    }
+    public static class A implements Closeable {
+        @Override
+        public void close() throws IOException {
+            System.out.println("关了");
+        }
     }
 }
